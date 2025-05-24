80e46566d6e06bb0d2bfd233c2826c14fef4fddb
Lucas <mrpiratex35@gmail.com>
2025-05-24 01:06:15 -0300

    silly program i came up with idk

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+/*
+---------------------------------------
+C++ IS BASED AF
+NORMIES GET THE FUCK OUT!!11
+
+MAY, 24TH 2025.
+---------------------------------------
+*/
+
+
+int main(){
+
+    std::cout << "Enter the length of your penis: ";
+    int x {};
+    std::cin >> x;
+
+    if (x != 24){
+
+        std::cout << "Wow that's a long penis! Here's a visual representation:\n";
+
+        std::cout << "<";
+
+        for (int i = 0; i < x; i++){
+            std::cout << "=";
+        }
+
+        std::cout << "3\n";
+    
+    } else {
+        std::cout << "You're gay as hell, bro lmao\n";
+    }
+
+    return 0;
+}
